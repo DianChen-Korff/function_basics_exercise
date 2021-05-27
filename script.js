c128897c78693d890c08a5c41092dc3f15775de0
DianChen-Korff <79322059+DianChen-Korff@users.noreply.github.com>
2021-05-27 15:08:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Writing or "defining" a function
+
+const makePizza = function () {
+    console.log ("Make pizza dough");
+    console.log ("Put on tomato sauce");
+    console.log ("Put on toppings");
+    console.log ("Top it off with grated cheese");
+    console.log ("Put pizza in oven");
+};
+
+// Using a function
+makePizza();
+makePizza();
+makePizza();
+
+const makeSushi = function () {
+    console.log ("Cook sushi rice");
+    console.log ("Add vineger by taste");
+    console.log ("Chop cucumber");
+    console.log ("Combine rice and sushi rice");
+    console.log ("Roll into large roll");
+    console.log ("Cut into small pieces")
+};
+
+makePizza();
+makePizza();
+makePizza();
+makeSushi();
+makeSushi();
